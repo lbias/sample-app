f08ad9c8f9ac967fb6399e9000c61c0b6918f99b
lbias <306508354@qq.com>
2017-08-09 15:52:36 +0800

    has_secure_password, add password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
   before_save { self.email = email.downcase }
+
+  has_secure_password
 end
