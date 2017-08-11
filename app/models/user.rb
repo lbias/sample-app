9f999bd47e3c78602a385d2b760d5acc70e6dd73
lbias <306508354@qq.com>
2017-08-11 11:34:13 +0800

    remember a suer in the database for use in persistent sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
   before_save { self.email = email.downcase }
@@ -17,5 +18,11 @@ class User < ApplicationRecord
   # returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # remember a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
