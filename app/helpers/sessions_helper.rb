33fd6243823b8d86608fc5df660c8b67cf14eace
lbias <306508354@qq.com>
2017-08-11 17:23:54 +0800

    friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
   # Returns true if the given user is the current user.
   def current_user?(user)
     user == current_user
-  end  
+  end
 
   #returns the user corresponding to the remember token cookie
   def current_user
@@ -47,4 +47,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end  
 end
