4492e3a53eb2751717c93436b044c1db911b32f6
lbias <306508354@qq.com>
2017-08-11 11:43:35 +0800

    define the new token and digest methods using class<<self

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,18 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }
 
-  #returns the hash digest of the given string
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  class << self
+    #returns the hash digest of the given string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # returns a random token
-  def self.new_token
-    SecureRandom.urlsafe_base64
+    # returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # remember a user in the database for use in persistent sessions
