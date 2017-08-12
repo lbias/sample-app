57dec298d999c108f8947b4c87af1169559e4392
lbias <306508354@qq.com>
2017-08-12 23:05:40 +0800

    test some following methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                 foreign_key: "follower_id",
                                 dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
 
   class << self
     #returns the hash digest of the given string
@@ -77,6 +78,21 @@ class User < ApplicationRecord
   # see "following users" for the full implementation
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
