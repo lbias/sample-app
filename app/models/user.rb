4a5a3d8eb064a5e4799bb87694a9e71a0adf6879
lbias <306508354@qq.com>
2017-08-12 15:40:37 +0800

    association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }, allow_blank: true
 
+  has_many :microposts
+
   class << self
     #returns the hash digest of the given string
     def digest(string)
