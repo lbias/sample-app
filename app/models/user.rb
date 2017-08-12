8b4e17e4073a3ef5102e0e3c8522b84d942c8bd9
lbias <306508354@qq.com>
2017-08-12 15:52:39 +0800

    the dependent microposts to be destroyed along with the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }, allow_blank: true
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   class << self
     #returns the hash digest of the given string
